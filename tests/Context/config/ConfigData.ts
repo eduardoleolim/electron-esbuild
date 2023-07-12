d4de9698e3f4ab5b3b29b9e3de6a808b837430be
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-07-12 02:29:54 -0600

    test(output-config): test fromJson()

---
+++ tests/Context/config/ConfigData.ts
@@ -0,0 +1,4 @@
+export const validConfigData = '{ "directory": "value", "filename": "value2" }';
+export const invalidDirectoryConfigData = '{ "directory": 123, "filename": "value2" }';
+export const invalidAbsolutePathConfigData = '{ "directory": "d://eduardoleolim", "filename": "value2" }';
+export const invalidFilenameConfigData = '{ "directory": "value", "filename": 123 }';
---
+++ tests/Context/config/OutputConfig.test.ts
@@ -0,0 +1,54 @@
+import { OutputConfig } from '../../../src/Context/config/domain/OutputConfig';
+import {
+  invalidAbsolutePathConfigData,
+  invalidDirectoryConfigData,
+  invalidFilenameConfigData,
+  validConfigData,
+} from './ConfigData';
+
+describe('OutputConfig module', () => {
+  test('Parse from json', () => {
+    try {
+      const jsonParsed = JSON.parse(validConfigData);
+      const outputConfig = OutputConfig.fromJson(jsonParsed);
+
+      expect(outputConfig.directory).toBe(jsonParsed.directory);
+      expect(outputConfig.filename).toBe(jsonParsed.filename);
+    } catch (error: any) {
+      throw new Error(error.message);
+    }
+  });
+
+  test('Invalid directory', () => {
+    try {
+      const jsonParsed = JSON.parse(invalidDirectoryConfigData);
+      OutputConfig.fromJson(jsonParsed);
+
+      expect(true).toBe(false);
+    } catch (error: any) {
+      expect(error.message).toBe('OutputConfig.fromJson: directory must be a string');
+    }
+  });
+
+  test('Invalid absolute path', () => {
+    try {
+      const jsonParsed = JSON.parse(invalidAbsolutePathConfigData);
+      OutputConfig.fromJson(jsonParsed);
+
+      expect(true).toBe(false);
+    } catch (error: any) {
+      expect(error.message).toBe('OutputConfig.fromJson: directory must be a relative path');
+    }
+  });
+
+  test('Invalid filename', () => {
+    try {
+      const jsonParsed = JSON.parse(invalidFilenameConfigData);
+      OutputConfig.fromJson(jsonParsed);
+
+      expect(true).toBe(false);
+    } catch (error: any) {
+      expect(error.message).toBe('OutputConfig.fromJson: filename must be a string');
+    }
+  });
+});
