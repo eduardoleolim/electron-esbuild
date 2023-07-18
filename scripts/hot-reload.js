4dbd146c167e9cc357811382e8ada08a03c5d152
Angel Eduardo Martínez Leo Lim <eduardoleolim@hotmail.com>
2023-07-18 02:11:39 -0600

    refactor(esbuild-dev): remove unnecessary livereload server
    Use esbuild live reload approach. It's more simple and works better.
    
    The hot-reload script is copied to the output directory, it reloads the page when the renderer process is rebuilt.

---
+++ scripts/hot-reload.js
@@ -0,0 +1,28 @@
+// https://esbuild.github.io/api/#hot-reloading-css
+const hotReload = new EventSource('/esbuild');
+
+hotReload.addEventListener('change', (event) => {
+  const { added, removed, updated } = JSON.parse(event.data);
+
+  if (added.length === 0 && removed.length === 0) {
+    /**
+     * @type {HTMLCollectionOf<HTMLLinkElement & Node>}
+     */
+    const links = document.getElementsByTagName('link');
+    for (const link of links) {
+      const url = new URL(link.href);
+
+      for (const resource of updated) {
+        if (url.host !== location.host && url.pathname !== resource) continue;
+
+        const next = link.cloneNode();
+        next.href = resource + '?' + Math.random().toString(36).slice(2);
+        next.onload = () => link.remove();
+        link.parentNode?.insertBefore(next, link.nextSibling);
+        return;
+      }
+    }
+  }
+
+  location.reload();
+});
--- src/Context/builder/infrastructure/services/RendererEsbuildElectronBuilder.ts
+++ src/Context/builder/infrastructure/services/RendererEsbuildElectronBuilder.ts
@@ -4,9 +4,6 @@ import path from 'path';
 import fs from 'fs';
 import { MainConfig } from '../../../config/domain/MainConfig';
 import { findFreePort } from '../../../shared/infrastructure/findFreePort';
-import chokidar from 'chokidar';
-import { RendererProcessServer } from './RendererProcessServer';
-import { getDependencies } from '../../../shared/infrastructure/getDependencies';
 
 export class RendererEsbuildElectronBuilder {
   private readonly mainConfig: MainConfig;
@@ -28,70 +25,40 @@ export class RendererEsbuildElectronBuilder {
   }
 
   public async build(): Promise<void> {
+    const outputDirectory = path.resolve(
+      this.outputDirectory,
+      this.rendererConfig.output?.directory ?? this.mainConfig.output.directory,
+    );
+
     console.log('Building renderer process...');
     const buildOptions = this.prepareBuildOptions();
     const context = await esbuild.context<BuildOptions>(buildOptions);
     await context.rebuild();
     await context.cancel();
     await context.dispose();
-    this.copyHtml();
+    this.copyHtml(outputDirectory);
     console.log('Renderer process built');
   }
 
   public async dev(): Promise<void> {
-    const buildOptions = this.prepareBuildOptions();
-    const context = await esbuild.context<BuildOptions>(buildOptions);
     const host = '127.0.0.1';
-    const portContext = await findFreePort(8000);
+    const port = this.rendererConfig.devPort || (await findFreePort(8000));
     const servedir = path.resolve(
       this.outputDirectory,
       this.rendererConfig.output?.directory ?? this.mainConfig.output.directory,
     );
-    const htmlPath = path.resolve(this.rendererConfig.html);
-    const outHtml = path.resolve(servedir, path.basename(htmlPath));
-    const outCss = path.resolve(
-      path.dirname(this.outRendererFile),
-      path.basename(this.outRendererFile, '.js') + '.css',
-    );
-    const relativeRendererScriptPath = path.relative(path.dirname(outHtml), this.outRendererFile);
 
-    function calculateCssRelativePath(): string | undefined {
-      if (!fs.existsSync(outCss)) return undefined;
+    const buildOptions = this.prepareBuildOptions();
+    const context = await esbuild.context<BuildOptions>(buildOptions);
 
-      return path.relative(path.dirname(outHtml), outCss);
-    }
+    await context.rebuild();
+    await context.watch();
+    await context.serve({ port, host, servedir });
+    this.copyHtml(servedir);
 
-    console.log('Building renderer process...');
-    context.serve({ port: portContext, host, servedir }).then(async (result) => {
-      console.log('Renderer process built');
-
-      const server = new RendererProcessServer(result, htmlPath, relativeRendererScriptPath, calculateCssRelativePath);
-
-      let sources = getDependencies(path.resolve(this.rendererConfig.entry));
-      const watcher = chokidar.watch(sources, { disableGlobbing: false });
-
-      watcher.on('ready', () => {
-        watcher.on('all', async (eventName, file) => {
-          server.reload(file);
-
-          const newSources = getDependencies(path.resolve(this.rendererConfig.entry));
-          const removedSources = sources.filter((s) => !newSources.includes(s));
-          const addedSources = newSources.filter((s) => !sources.includes(s));
-          sources = newSources;
-
-          watcher.unwatch(removedSources);
-          watcher.add(addedSources);
-        });
-      });
-
-      process.on('exit', async () => {
-        server.stop();
-        await context.cancel();
-        await context.dispose();
-      });
-
-      const devPort = await findFreePort(this.rendererConfig.devPort || 8000);
-      server.start(devPort, host);
+    process.on('exit', async () => {
+      await context.cancel();
+      await context.dispose();
     });
   }
 
@@ -128,21 +95,17 @@ export class RendererEsbuildElectronBuilder {
     };
   }
 
-  private copyHtml(): void {
+  private copyHtml(directory: string): void {
     const html = path.resolve(this.rendererConfig.html);
-    const outDir = path.resolve(
-      this.outputDirectory,
-      this.rendererConfig.output?.directory ?? this.mainConfig.output.directory,
-    );
-    const outHtml = path.resolve(outDir, path.basename(html));
+    const outHtml = path.resolve(directory, path.basename(html));
     const relativeRendererScriptPath = path.relative(path.dirname(outHtml), this.outRendererFile);
 
     if (!fs.existsSync(html)) {
       throw new Error(`Html file not found in <${html}>`);
     }
 
-    if (!fs.existsSync(outDir)) {
-      fs.mkdirSync(outDir, { recursive: true });
+    if (!fs.existsSync(directory)) {
+      fs.mkdirSync(directory, { recursive: true });
     }
 
     let content = fs.readFileSync(html, 'utf-8').replace(
@@ -150,6 +113,15 @@ export class RendererEsbuildElectronBuilder {
       `  <script src="${relativeRendererScriptPath}"></script>
   </body>`,
     );
+
+    if (process.env.NODE_ENV === 'development') {
+      content = content.replace(
+        '</body>',
+        `  <script src="hot-reload.js"></script>
+  </body>`,
+      );
+      this.copyHotReload(directory);
+    }
 
     const outCss = path.resolve(
       path.dirname(this.outRendererFile),
@@ -162,10 +134,16 @@ export class RendererEsbuildElectronBuilder {
       content = content.replace(
         '</head>',
         `  <link rel="stylesheet" href="${outCssRelative}">
-</head>`,
+  </head>`,
       );
     }
 
     fs.writeFileSync(outHtml, content, 'utf-8');
+  }
+
+  private copyHotReload(directory: string): void {
+    const inputPathScript = path.resolve(__dirname, '..', '..', '..', '..', '..', 'scripts', 'hot-reload.js');
+    const outputPathScript = path.resolve(directory, 'hot-reload.js');
+    fs.copyFileSync(inputPathScript, outputPathScript);
   }
 }
