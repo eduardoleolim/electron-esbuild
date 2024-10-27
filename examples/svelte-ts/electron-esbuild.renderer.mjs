0e96483a4fd2967b3d13af4460a1ef22aa38de22
Eduardo Martínez <eduardoleolim@hotmail.com>
2024-10-27 03:33:06 -0600

    chore: update examples

--- examples/svelte-ts/electron-esbuild.renderer.mjs
+++ examples/svelte-ts/electron-esbuild.renderer.mjs
@@ -1,14 +1,16 @@
 import esbuild from 'esbuild';
-import sveltePlugin from "esbuild-svelte";
-import sveltePreprocess from "svelte-preprocess";
+import sveltePlugin from 'esbuild-svelte';
+import { sveltePreprocess } from 'svelte-preprocess';
 
 /**
  * @type {esbuild.BuildOptions}
  */
 const config = {
-    plugins: [sveltePlugin({
+  plugins: [
+    sveltePlugin({
       preprocess: sveltePreprocess(), // Necessary for TypeScript
-    })]
-}
+    }),
+  ],
+};
 
 export default config;
