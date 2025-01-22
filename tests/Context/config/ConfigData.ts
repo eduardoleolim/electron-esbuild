26d755555b540f7ac71921dc7d0abf00ea34954c
Eduardo Martínez <eduardoleolim@hotmail.com>
2025-01-22 02:03:40 -0600

    test(config-data): update valid and invalid output config data

--- tests/Context/config/ConfigData.ts
+++ tests/Context/config/ConfigData.ts
@@ -1,4 +1,4 @@
-export const validConfigData = '{ "directory": "value", "filename": "value2" }';
+export const validOutputConfig = '{ "directory": "out_dir", "filename": "out_file.ext" }';
 
 export const invalidDirectoryConfigData = '{ "directory": 123, "filename": "value2" }';
 
@@ -7,3 +7,9 @@ const absolutePath = process.platform === 'win32' ? 'd:\\\\eduardoleolim' : '/ed
 export const invalidAbsolutePathConfigData = `{ "directory": "${absolutePath}", "filename": "value2" }`;
 
 export const invalidFilenameConfigData = '{ "directory": "value", "filename": 123 }';
+
+export const invalidFromResourcesConfigData = '{ "from": 123, "to": "value2" }';
+
+export const invalidToResourcesConfigData = '{ "from": "value", "to": 123 }';
+
+export const validCustomResourceConfigData = `{ "from": "from/file.ext", "to": ${validOutputConfig} }`;
